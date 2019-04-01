034da5186276ed98c077a106ddad871c3a9de16e
Dan Choiniere <dchoinie@gmail.com>
2019-04-01 18:52:05 -0500

    Initial Commit
    Components Created: Artists, Artist

---
+++ src/components/Artist.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>{{artist.Date}}</p>
+        <p>{{artist.Weekday}}</p>
+        <p>{{artist.Artist}}</p>
+        <p>{{artist.Price}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Artist",
+        props: ["artist"]
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Artists.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-bind:key="artist.id" v-for="artist in artists" >
+            <Artist v-bind:artist="artist" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Artist from './Artist.vue';
+
+export default {
+    name: "Artists",
+    components: {
+        Artist
+    },
+    props: ["artists"]
+}
+</script>
+
+<style scoped>
+
+</style>
