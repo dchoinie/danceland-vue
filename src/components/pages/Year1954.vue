ea01f3e85219c075377c4227c89ce34226e4da5d
Dan Choiniere <dchoinie@gmail.com>
2019-04-10 19:21:22 -0500

    1954 Year Data Trial
    Cleared firestore data. Entered all image locations. Re-populated firestore. Display data on year/1954 page. Ordered by ID.
    
    *Still can't .orderBY() & .where()

--- src/components/pages/Year1954.vue
+++ src/components/pages/Year1954.vue
@@ -1,17 +1,56 @@
 <template>
     <div>
-        <h1>1954</h1>
-        <div class="container">
-            <!-- <ul class="artist-group text-center">
-                <li v-for="artist in artists" v-bind:key="artist.id" class="artist-group-item">
+        <div class="container center-align">
+            <h2>1954</h2>
+            <p>National acts that played at Danceland in 1954 include - Wayne King & His Orchestra,
+            “Whoopee” John Wilfahrt & His Orchestra, WNAX Bohemian Band, Fezz Fritsche & His
+            Goosetown Band, Six Fat Dutchmen, Frank Yankovic & His Yanks</p>
+            <ul class="artist-group text-center">
+                <li v-for="artist in artists" v-bind:key="artist.id" class="artist-group-item pt-3">
                     <p>{{ artist.year }}</p>
                     <p>{{ artist.date }}, {{ artist.weekday }}</p>
                     <p>{{ artist.artist }}</p>
                     <p>{{ artist.price }}</p>
-                    <img src="../../assets/1954/front.jpg" alt="artistImg" class="artistImg">
+                    <div class="imageGallery pb-3">
+                        <img :src="artist.img1" alt="artistImg" class="artistImg">
+                        <img :src="artist.img2" alt="artistImg2" class="artistImg">
+                        <img :src="artist.img3" alt="artistImg3" class="artistImg">
+                        <img :src="artist.img4" alt="artistImg4" class="artistImg">
+                        <img :src="artist.img5" alt="artistImg5" class="artistImg">
+                        <img :src="artist.img6" alt="artistImg6" class="artistImg">
+                        <img :src="artist.img7" alt="artistImg7" class="artistImg">
+                        <img :src="artist.img8" alt="artistImg8" class="artistImg">
+                        <img :src="artist.img9" alt="artistImg9" class="artistImg">
+                        <img :src="artist.img10" alt="artistImg10" class="artistImg">
+                        <img :src="artist.img11" alt="artistImg11" class="artistImg">
+                        <img :src="artist.img12" alt="artistImg12" class="artistImg">
+                        <img :src="artist.img13" alt="artistImg13" class="artistImg">
+                        <img :src="artist.img14" alt="artistImg14" class="artistImg">
+                        <img :src="artist.img15" alt="artistImg15" class="artistImg">
+                        <img :src="artist.img16" alt="artistImg16" class="artistImg">
+                        <img :src="artist.img17" alt="artistImg17" class="artistImg">
+                        <img :src="artist.img18" alt="artistImg18" class="artistImg">
+                        <img :src="artist.img19" alt="artistImg19" class="artistImg">
+                        <img :src="artist.img20" alt="artistImg20" class="artistImg">
+                        <img :src="artist.img21" alt="artistImg21" class="artistImg">
+                        <img :src="artist.img22" alt="artistImg22" class="artistImg">
+                        <img :src="artist.img23" alt="artistImg23" class="artistImg">
+                        <img :src="artist.img24" alt="artistImg24" class="artistImg">
+                        <img :src="artist.img25" alt="artistImg25" class="artistImg">
+                        <img :src="artist.img26" alt="artistImg26" class="artistImg">
+                        <img :src="artist.img27" alt="artistImg27" class="artistImg">
+                        <img :src="artist.img28" alt="artistImg28" class="artistImg">
+                        <img :src="artist.img29" alt="artistImg29" class="artistImg">
+                        <img :src="artist.img30" alt="artistImg30" class="artistImg">
+                        <img :src="artist.img31" alt="artistImg31" class="artistImg">
+                        <img :src="artist.img32" alt="artistImg32" class="artistImg">
+                        <img :src="artist.img33" alt="artistImg33" class="artistImg">
+                        <img :src="artist.img34" alt="artistImg34" class="artistImg">
+                        <img :src="artist.img35" alt="artistImg35" class="artistImg">
+                    </div>
                     <hr class="style-eight" />
                 </li>
-            </ul> -->
+            </ul>
         </div>
     </div>
 </template>
@@ -27,7 +66,8 @@ import db from '../firebaseInit'
         },
         created() {
         var artistsRef = db.collection('artists');
-        var query = artistsRef.where('year', '==', 1954).get()
+        // var query = artistsRef.where('year', '==', 1954).get()
+        var query = artistsRef.orderBy("ID").get()
         .then(snapshot => {
             if (snapshot.empty) {
             console.log('No matching documents.');
@@ -37,12 +77,47 @@ import db from '../firebaseInit'
             snapshot.forEach(doc => {
                 const data = {
                     'id': doc.id,
-                    'date': doc.data().date,
-                    'weekday': doc.data().weekday,
-                    'artist': doc.data().artist,
-                    'price': doc.data().price,
-                    'year': doc.data().year,
-                    'imgLocation': doc.data().ImgLocation
+                    'customID': doc.data().ID,
+                    'date': doc.data().Date,
+                    'weekday': doc.data().Weekday,
+                    'artist': doc.data().Artist,
+                    'price': doc.data().Price,
+                    'year': doc.data().Year,
+                    'img1': doc.data().Img1,
+                    'img2': doc.data().Img2,
+                    'img3': doc.data().Img3,
+                    'img4': doc.data().Img4,
+                    'img5': doc.data().Img5,
+                    'img6': doc.data().Img6,
+                    'img7': doc.data().Img7,
+                    'img8': doc.data().Img8,
+                    'img9': doc.data().Img9,
+                    'img10': doc.data().Img10,
+                    'img11': doc.data().Img11,
+                    'img12': doc.data().Img12,
+                    'img13': doc.data().Img13,
+                    'img14': doc.data().Img14,
+                    'img15': doc.data().Img15,
+                    'img16': doc.data().Img16,
+                    'img17': doc.data().Img17,
+                    'img18': doc.data().Img18,
+                    'img19': doc.data().Img19,
+                    'img20': doc.data().Img20,
+                    'img21': doc.data().Img21,
+                    'img22': doc.data().Img22,
+                    'img23': doc.data().Img23,
+                    'img24': doc.data().Img24,
+                    'img25': doc.data().Img25,
+                    'img26': doc.data().Img26,
+                    'img27': doc.data().Img27,
+                    'img28': doc.data().Img28,
+                    'img29': doc.data().Img29,
+                    'img30': doc.data().Img30,
+                    'img31': doc.data().Img31,
+                    'img32': doc.data().Img32,
+                    'img33': doc.data().Img33,
+                    'img34': doc.data().Img34,
+                    'img35': doc.data().Img35
                 }
                 console.log(data)
                 this.artists.push(data)
@@ -56,6 +131,9 @@ import db from '../firebaseInit'
 </script>
 
 <style scoped>
+    img[src=""] {
+        display: none;
+    }
     ul {
         list-style-type: none;
     }
